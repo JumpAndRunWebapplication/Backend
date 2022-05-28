719e35e85be429354e5d905a4f7376a8c85feb00
PRSoftRafael <bakaschevr@gmail.com>
2022-05-28 19:36:38 +0200

    changes in cors filter to host this application on a server

--- src/main/java/pr/jku/backend/jump/run/Application.java
+++ src/main/java/pr/jku/backend/jump/run/Application.java
@@ -21,6 +21,7 @@ public class Application {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
 		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+//		corsConfiguration.setAllowedOrigins(Arrays.asList("http://runjump.system2020.at"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Request-Headers"));
 		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
